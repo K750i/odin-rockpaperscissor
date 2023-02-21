0c339c2935b8ecad29ddacb586d16c4f091bf290
K750i <munkin80@gmail.com>
2023-02-21 13:28:58 +0800

    Add UI to the game

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('main>button')
+
+const playGame = function () {
+  game(this.dataset.choice)
+}
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', playGame)
+})
+
+
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissor']
-    return choices[Math.floor(Math.random() * 3)]
+  const choices = ['rock', 'paper', 'scissor']
+  return choices[Math.floor(Math.random() * 3)]
 }
 
 function playRound(playerSelection, computerSelection) {
-    const conditions = new Map([['Rock', 'Scissor'], ['Scissor', 'Paper'], ['Paper', 'Rock']])
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
-    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1).toLowerCase()
+  const conditions = new Map([['Rock', 'Scissor'], ['Scissor', 'Paper'], ['Paper', 'Rock']])
+  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+  computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1).toLowerCase()
 
-    if (playerSelection === computerSelection) {
-        return [0, "It's a tie!"]
-    } else if (conditions.get(playerSelection) === computerSelection) {
-        return [1, `You Win! ${playerSelection} beats ${computerSelection}`]
-    } else {
-        return [-1, `You Lose! ${computerSelection} beats ${playerSelection}`]
-    }
+  if (playerSelection === computerSelection) {
+    return [0, "It's a tie!"]
+  } else if (conditions.get(playerSelection) === computerSelection) {
+    return [1, `You Win! ${playerSelection} beats ${computerSelection}`]
+  } else {
+    return [-1, `You Lose! ${computerSelection} beats ${playerSelection}`]
+  }
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
+function game(playerInput) {
+  const [pts, msg] = playRound(playerInput, getComputerChoice())
+  if (pts === 0) {
+    displayResult(playerScore, computerScore, msg)
+  } else if (pts > 0) {
+    playerScore++
+    displayResult(playerScore, computerScore, msg)
+  } else {
+    computerScore++
+    displayResult(playerScore, computerScore, msg)
+  }
+  // }
+  if (playerScore >= 5) {
+    displayResult(playerScore, computerScore, msg, `You won the game by ${playerScore} to ${computerScore}`)
+    buttons.forEach(btn => {
+      btn.removeEventListener('click', playGame)
+    })
+  }
 
-    for (let i = 1; i < 6; i++) {
-        console.log(`Round ${i}`)
-        const playerInput = prompt('Rock, Paper, or Scissor?')
-        const [pts, msg] = playRound(playerInput, getComputerChoice())
-        if (pts === 0) {
-            console.log(msg)
-        } else if (pts > 0) {
-            playerScore++
-            console.log(msg)
-        } else {
-            computerScore++
-            console.log(msg)
-        }
-    }
-
-    if (playerScore > computerScore) {
-        console.log(`You won the game by ${playerScore} to ${computerScore}`)
-    } else {
-        console.log(`You lose the game by ${computerScore} to ${playerScore}`);
-    }
+  if (computerScore >= 5) {
+    displayResult(playerScore, computerScore, msg, `You lose the game by ${computerScore} to ${playerScore}`)
+    buttons.forEach(btn => {
+      btn.removeEventListener('click', playGame)
+    })
+  }
 }
+
+function displayResult(player, computer, msg, gameoverMsg) {
+  const msgDisplay = document.querySelector('.outcome')
+  const playerScoreDisplay = document.querySelector('.player_score')
+  const computerScoreDisplay = document.querySelector('.computer_score')
+  const gameoverDisplay = document.querySelector('.gameover')
+
+  playerScoreDisplay.textContent = player
+  computerScoreDisplay.textContent = computer
+  msgDisplay.textContent = msg
+  gameoverDisplay.textContent = gameoverMsg
+}
+
+
